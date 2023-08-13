4757771014f2ad5077b6cda21121deba5e24f03c
Muhammad Manan Nasir <mannannasir49@gmail.com>
2023-08-13 19:41:50 +0500

    Changes boolean variable of protected route

--- src/functions/SharedDataContext.js
+++ src/functions/SharedDataContext.js
@@ -5,7 +5,7 @@ export const SharedDataContext = createContext();
 export const SharedDataProvider = ({ children }) => {
   const [chapter, setChapter] = useState("");
   const [filteredChapters, setFilteredChapters] = useState([]);
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
     <SharedDataContext.Provider
